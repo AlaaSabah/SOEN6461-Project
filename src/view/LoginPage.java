6f04e6e219f2f69811e0a98340c636626560cf65
AlaaSabah <alaa.m.sabbah@gmail.com>
2017-02-21 13:45:15 -0800

    Login page done

--- src/view/LoginPage.java
+++ src/view/LoginPage.java
@@ -1,15 +1,24 @@
 package view;
-
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LoginPage extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField nameTxt;
+	private JTextField passTxt;
 
 	/**
 	 * Launch the application.
@@ -31,12 +40,54 @@ public class LoginPage extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginPage() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 604, 418);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Name");
+		lblNewLabel_1.setForeground(new Color(255, 0, 0));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_1.setBounds(286, 247, 63, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Password");
+		lblNewLabel_2.setForeground(new Color(255, 0, 0));
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_2.setBounds(286, 289, 63, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		nameTxt = new JTextField();
+		nameTxt.setBounds(388, 245, 129, 20);
+		contentPane.add(nameTxt);
+		nameTxt.setColumns(10);
+		
+		passTxt = new JTextField();
+		passTxt.setBounds(388, 287, 129, 20);
+		contentPane.add(passTxt);
+		passTxt.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\alaa\\Desktop\\Master\\EclipseProjects\\SOEN6461Project\\src\\images\\login 3.png"));
+		lblNewLabel_3.setBounds(286, 11, 233, 236);
+		contentPane.add(lblNewLabel_3);
+		
+		JButton btnNewButton = new JButton("Log in");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBackground(new Color(240, 240, 240));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnNewButton.setBounds(411, 328, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\alaa\\Desktop\\Master\\EclipseProjects\\SOEN6461Project\\src\\images\\login background.jpg"));
+		lblNewLabel.setBounds(0, 0, 588, 379);
+		contentPane.add(lblNewLabel);
 	}
-
 }
